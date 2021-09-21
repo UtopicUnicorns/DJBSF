33070c0512fd508109872c0e11e02018dd2ce0fb
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2021-09-21 17:15:42 +0200

    Visuals

---
+++ visuals_non_core/website/scripts/slider_size.js
@@ -0,0 +1,21 @@
+var slider_size = document.getElementById('slider_size');
+var content_holder = document.getElementById('content_holder');
+var bubble = document.getElementById('bubble');
+
+slider_size.addEventListener('input', function () {
+	content_holder.style['font-size'] = this.value + 'px';
+	setBubble(this.value, bubble);
+});
+
+setBubble(slider_size.value, bubble);
+
+function setBubble(range, bubble) {
+	const val = slider_size.value;
+	const min = slider_size.min ? slider_size.min : 0;
+	const max = slider_size.max ? slider_size.max : 100;
+	const newVal = Number(((val - min) * 100) / (max - min));
+	bubble.innerHTML = `${val} px`;
+
+	// Sorta magic numbers based on size of the native UI thumb
+	bubble.style.left = `calc(${newVal}% + (${0 - newVal * 0.5}px))`;
+}
