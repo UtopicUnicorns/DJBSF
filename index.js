c59b00b584077fc9dcda866b2284c55c7abf3438
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2021-12-01 17:02:32 +0100

    Small update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*Start in async to check for errors*/
 async function start() {
 	try {
-		await require('./djbsf_library/index').lib('./configs.json'); //Import library + path to your config
+		await require('./library/index').lib('./configs.json'); //Import library + path to your config
 	} catch (err) {
 		console.log(err);
 	}
