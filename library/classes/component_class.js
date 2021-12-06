383f2ba58fce548e17a2bf596eb2cddaa8050c05
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2021-12-06 15:56:28 +0100

    Update component_class.js

--- library/classes/component_class.js
+++ library/classes/component_class.js
@@ -4,11 +4,38 @@ class component_construct {
 			type: 1,
 			components: [],
 		};
-
 	}
 
 	get output() {
 		return this.data;
+	}
+
+	menu() {
+		let menu_info = {
+			custom_id: '32231', //string
+			placeholder: '5544', //string
+			options: [
+				{
+					label: '3454343', //string
+					value: '53453434', //string
+					description: '342342342', //string
+					default: false, //coolean
+				},
+				{
+					label: '2334234', //string
+					value: '23432324', //string
+					description: '234432342342', //string
+					default: false, //boolean
+				},
+			],
+			min_values: 1, //int
+			max_values: 1, //int
+			type: 3, //type
+		};
+
+		this.data.components.push(menu_info);
+
+		return this;
 	}
 
 	button(label, custom, style, disabled, url) {
@@ -26,7 +53,8 @@ class component_construct {
 		if (!style) delete button_info['style'];
 		if (!disabled) button_info['disabled'] = false;
 		if (!url) delete button_info['url'];
-		if (style === 5) delete button_info['custom_id'];
+		if (url) button_info['style'] = 5;
+		if (url) delete button_info['custom_id'];
 
 		this.data.components.push(button_info);
 
