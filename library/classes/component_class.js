35d35fd4ff14fd217fe517a3f760563640bb5617
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2022-01-26 17:09:53 +0100

    FIX BUTTONS!
    FIX BUTTONS!FIX BUTTONS!FIX BUTTONS!FIX BUTTONS!FIX BUTTONS!

--- library/classes/component_class.js
+++ library/classes/component_class.js
@@ -1,9 +1,11 @@
 class component_construct {
 	constructor() {
-		this.menucount = 0;
-		this.buttoncount = 0;
 		this.menuBOOL = false;
 		this.buttonBOOL = false;
+		this.button = {
+			type: 1,
+			components: [],
+		};
 
 		this.data = {
 			type: 1,
@@ -12,36 +14,28 @@ class component_construct {
 	}
 
 	get output() {
-		if (this.menuBOOL) console.log('True');
-		if (!this.menuBOOL) console.log('False');
-		return this.data;
+		if (this.menuBOOL) {
+			return this.data;
+		}
+
+		if (this.buttonBOOL) {
+			return this.button;
+		}
 	}
 
-	menu() {
+	menu(menudata) {
+		if (!menudata) return 'No information.';
+
 		if (this.buttonBOOL) return this;
-		menucount++;
 		this.menuBOOL = true;
 
 		let menu_info = {
-			custom_id: '32231', //string
-			placeholder: '5544', //string
-			options: [
-				{
-					label: '3454343', //string
-					value: '53453434', //string
-					description: '342342342', //string
-					default: false, //coolean
-				},
-				{
-					label: '2334234', //string
-					value: '23432324', //string
-					description: '234432342342', //string
-					default: false, //boolean
-				},
-			],
-			min_values: 1, //int
-			max_values: 1, //int
-			type: 3, //type
+			custom_id: menudata.custom_id,
+			placeholder: menudata.place_holder,
+			min_values: menudata.min_val,
+			max_values: menudata.max_val,
+			options: [],
+			type: 3,
 		};
 
 		this.data.components.push(menu_info);
@@ -49,9 +43,43 @@ class component_construct {
 		return this;
 	}
 
+	entry(entrydata) {
+		if (!entrydata) return 'No information.';
+
+		if (entrydata.emoji) {
+			var entry_info = {
+				label: entrydata.label,
+				value: entrydata.value,
+				description: entrydata.description,
+				default: entrydata.default,
+				emoji: { name: entrydata.emoji.name, id: entrydata.emoji.id, animated: entrydata.emoji.boolean },
+			};
+		} else {
+			var entry_info = {
+				label: entrydata.label,
+				value: entrydata.value,
+				description: entrydata.description,
+				default: entrydata.default,
+			};
+		}
+
+		if (!entrydata.emoji) delete entry_info['emoji'];
+		if (entrydata.emoji) {
+			if (!entrydata.emoji.name) delete entry_info.emoji['name'];
+			if (!entrydata.emoji.id) delete entry_info.emoji['id'];
+			if (!entrydata.emoji.animated) delete entry_info.emoji['animated'];
+		}
+		if (!entrydata.default) delete entry_info['default'];
+		if (!entrydata.description) delete entry_info['description'];
+		if (!entrydata.value) delete entry_info['value'];
+		if (!entrydata.label) delete entry_info['label'];
+		if (this.data.components[0] && this.data.components[0].options) this.data.components[0].options.push(entry_info);
+
+		return this;
+	}
+
 	button(label, custom, style, disabled, url) {
 		if (this.menuBOOL) return this;
-		this.buttoncount++;
 		this.buttonBOOL = true;
 
 		let button_info = {
@@ -71,7 +99,7 @@ class component_construct {
 		if (url) button_info['style'] = 5;
 		if (url) delete button_info['custom_id'];
 
-		this.data.components.push(button_info);
+		this.button.components.push(button_info);
 
 		return this;
 	}
