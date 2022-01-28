1d955e9b123f35fd1d8aea2169b0bfcd0d5bb239
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2022-01-28 18:41:00 +0100

    Update heart_class.js

--- library/classes/heart_class.js
+++ library/classes/heart_class.js
@@ -7,13 +7,34 @@ class heart_construct {
 			client: null,
 			guilds: [],
 			users: [],
-			beat_interval: null,
+			beat_interval: 5000,
 			beating_heart: null,
 			seq_num: null,
 			ses_num: null,
 		};
 
+		this.identify_info = {
+			op: 2,
+			d: {
+				token: token,
+				intents: intents_num,
+				properties: {
+					$os: 'linux',
+					$browser: 'DJBSF',
+					$device: 'DJBSF',
+				},
+			},
+		};
+
+		mail_man.on('socket_beat', async () => {
+			this.client_struct.beating_heart++;
+			this.puppet.send(JSON.stringify({ op: 1, d: this.client_struct.seq_num }));
+		});
+		mail_man.on('socket_interval', async (info) => (this.client_struct.beat_interval = info));
 		mail_man.on('socket_close', async (code) => this.restart());
+		mail_man.on('socket_pulse', async (order) => this.pulse(order));
+		mail_man.on('socket_identify', async () => this.puppet.send(JSON.stringify(this.identify_info)));
+		mail_man.on('socket_ping', async () => this.puppet.send(JSON.stringify({ op: 1 })));
 		mail_man.on('socket_error', async (error) => console.log(error));
 	}
 
@@ -32,10 +53,30 @@ class heart_construct {
 		return this;
 	}
 
+	pulse(order) {
+		if (order) {
+			this.beat = setInterval(function () {
+				console.log('kek');
+				mail_man.emit('socket_beat');
+			}, this.client_struct.beat_interval);
+		} else clearInterval(this.beat);
+
+		return this;
+	}
+
 	socket() {
 		this.puppet.on('message', async function incoming(message) {
 			try {
 				const msg = JSON.parse(message);
+				if (msg.op == 10 && msg.d) {
+					mail_man.emit('socket_ping');
+					mail_man.emit('socket_identify');
+					mail_man.emit('socket_interval', msg.d.heartbeat_interval);
+				}
+
+				if (msg.op == 1) mail_man.emit('socket_ping');
+
+				//mail_man.emit('socket_pulse', true);
 
 				console.log(msg);
 			} catch (err) {
@@ -44,7 +85,10 @@ class heart_construct {
 		});
 
 		this.puppet.on('error', (error) => mail_man.emit('socket_error', error));
-		this.puppet.on('close', (code) => mail_man.emit('socket_close', code));
+		this.puppet.on('close', (code) => {
+			mail_man.emit('socket_close', code);
+			mail_man.emit('socket_pulse', false);
+		});
 
 		return this;
 	}
